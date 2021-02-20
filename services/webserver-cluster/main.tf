2e8a42b2889d025520f5eac80d9b12e4d1ebc62d
Ble <a.tanaphon@gmail.com>
2021-02-20 16:23:28 +0700

    modify asg to suppport custom tags

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -59,8 +59,18 @@ resource "aws_autoscaling_group" "example" {
 
   tag {
     key                 = "Name"
-    value               = "terraform-asg-example"
+    value               = "${var.cluster_name}-asg"
     propagate_at_launch = true
+  }
+
+  dynamic "tag" {
+    for_each = var.custom_tags
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
   }
 }
 
